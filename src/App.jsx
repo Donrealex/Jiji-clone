8c18113091d261529569b7e4388f191e8dabb2cc
Adebiyi Ridwan <donrealex11@gmail.com>
2025-10-14 13:10:05 +0100

    added text

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
+      something
       <h1 className="text-3xl font-bold text-yellow-200 underline">Hello world!</h1>
       <p>Wallywest in the building</p>
       <p>don in the building</p>
