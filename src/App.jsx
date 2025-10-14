fc151bc23c63ec8f84b8f6d1c78034886f39cd9f
Adebiyi Ridwan <donrealex11@gmail.com>
2025-10-14 15:15:14 +0100

    color configured

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-green-500 underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold text-primary bg-secondary underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
