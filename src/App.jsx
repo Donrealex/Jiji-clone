9d419aeb12f6b2886a7f4c057b6ece89f74d3f89
Wallychi <szetrov@gmail.com>
2025-10-14 12:49:36 +0100

    Added p tag

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold text-yellow-200 underline">Hello world!</h1>
+      <p>Wallywest in the building</p>
     </div>
   );
 }
