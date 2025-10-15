6248912937130675d9d1c301ec7386dd9799b6ee
Adebiyi Ridwan <donrealex11@gmail.com>
2025-10-15 17:55:41 +0100

    update on footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,213 +1,271 @@
+
 import React from "react";
-import { FaApple, FaGooglePlay } from "react-icons/fa";
-import footerCity from "../assets/footerCity.PNG";
 import { NavLink } from "react-router-dom";
+import footerCity from "../assets/footerCity.PNG";
 
 const Footer = () => {
   return (
-    <footer className="bg-primary text-white pb-8 mt-auto font-jiji">
+    <footer className="bg-primary items-baseline justify-center text-white  pb-8 mt-auto font-jiji">
       {/* Cityscape image */}
-      <div className="max-h-full w-full bg-[#ebf2f7] flex justify-center">
-        <img src={footerCity} alt="Cityscape" />
+      <div className="w-full bg-[#ebf2f7] flex justify-center">
+        <img
+          src={footerCity}
+          alt="Cityscape"
+          className="w-full max-w-screen-xl"
+        />
       </div>
-      <div className="container mx-auto px-4">
-        {/* Footer grid */}
-        <div className="grid grid-cols-1 md:grid-cols-5 gap-8 text-left mb-8">
+
+      <div className="pl-20 mx-auto px-4">
+        {/* Footer Grid */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-8 text-left mb-10 mt-6">
+          {/* About Us */}
           <div>
-            <h3 className="font-bold mb-4">About us</h3>
-            <ul className="gap-x-1">
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>About Jiji</li>
-              </NavLink>
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>We are hiring!</li>
-              </NavLink>
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>Terms & Conditions</li>
-              </NavLink>
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>Privacy Policy</li>
-              </NavLink>
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>Billing Policy</li>
-              </NavLink>
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>Candidate Privacy Policy</li>
-              </NavLink>
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>Cookie Policy</li>
-              </NavLink>
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>Copyright Infringement Policy</li>
-              </NavLink>
+            <h3 className="font-bold mb-4">About Us</h3>
+            <ul className="space-y-2">
+              {[
+                {
+                  text: "About Jiji",
+                  link: "https://jiji.ng/about.html",
+                },
+                {
+                  text: "We are hiring!",
+                  link: "https://jiji.ng/hiring.html",
+                },
+                {
+                  text: "Terms & Conditions",
+                  link: "https://jiji.ng/rules.html",
+                },
+                {
+                  text: "Privacy Policy",
+                  link: "https://jiji.ng/privacy.html",
+                },
+                {
+                  text: "Billing Policy",
+                  link: "https://jiji.ng/billing-policy.html",
+                },
+                {
+                  text: "Candidate Privacy Policy",
+                  link: "https://jiji.ng/jiji-pages/candidate-privacy-policy",
+                },
+                {
+                  text: "Cookie Policy",
+                  link: "https://jiji.ng/jiji-pages/cookie-policy",
+                },
+                {
+                  text: "Copyright Infringement Policy",
+                  link: "https://jiji.ng/jiji-pages/copyright-infringement-policy",
+                },
+              ].map((item, index) => (
+                <li key={index}>
+                  <NavLink
+                    to={item.link}
+                    className="hover:text-blue-900 transition"
+                  >
+                    {item.text}
+                  </NavLink>
+                </li>
+              ))}
             </ul>
           </div>
+
+          {/* Support */}
           <div>
             <h3 className="font-bold mb-4">Support</h3>
             <ul className="space-y-2">
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>support@jiji.ng</li>
-              </NavLink>
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>Safety tips</li>
-              </NavLink>
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>Contact Us</li>
-              </NavLink>
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>FAQ</li>
-              </NavLink>
+              {[
+                {
+                  text: "support@jiji.ng",
+                  link: "mailto:support@jiji.ng",
+                },
+                {
+                  text: "Safety tips",
+                  link: "https://jiji.ng/safety-tips.html",
+                },
+                {
+                  text: "Contact Us",
+                  link: "https://jiji.ng/contacts.html",
+                },
+                {
+                  text: "FAQ",
+                  link: "https://jiji.ng/faq",
+                },
+              ].map((item, index) => (
+                <li key={index}>
+                  <NavLink
+                    to={item.link}
+                    className="hover:text-blue-900 transition"
+                  >
+                    {item.text}
+                  </NavLink>
+                </li>
+              ))}
             </ul>
           </div>
-          <div className="md:col-span-1 flex flex-col items-center">
-            <h3 className="font-bold mb-4">Our apps</h3>
-            <a
-              href="https://apps.apple.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex flex-col items-center mb-4"
-            >
-              <div className="flex items-center justify-center ">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Download_on_the_App_Store_Badge.svg"
-                  alt="applestore"
-                />
-              </div>
-            </a>
-            <a
-              href="https://play.google.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex flex-col items-center"
-            >
-              <div className="flex items-center justify-center ">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
-                  alt="googleplay"
-                  className="w-35"
-                />
-              </div>
-            </a>
-          </div>
-          <div>
-            <h3 className="font-bold mb-4">Our resources</h3>
-            <ul className="space-y-2">
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>Our blog</li>
-              </NavLink>
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>Jiji on FB</li>
-              </NavLink>
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>Our Instagram</li>
-              </NavLink>
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>Our YouTube</li>
-              </NavLink>
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>Our Twitter</li>
-              </NavLink>
-            </ul>
-          </div>
-          <div>
-            <h3 className="font-bold mb-4">Hot links</h3>
-            <ul className="space-y-2">
-              <NavLink to="/" className="hover:text-blue-900">
-                <li>Jiji Africa</li>
-              </NavLink>
-            </ul>
-          </div>
-        </div>
-        {/* Flags */}
-        <div className="flex justify-center items-center gap-4 mb-4 flex-wrap">
-          <NavLink to="/">
-            <img
-              src="https://assets.jijistatic.net/static/img/footer-anchors/kenya.svg"
-              alt="kenya"
-              className="w-8 h-8 rounded-full border border-white"
-            />
-          </NavLink>
-          <NavLink to="/">
-            <img
-              src="https://assets.jijistatic.net/static/img/footer-anchors/uganda.svg"
-              alt="uganda"
-              className="w-8 h-8 rounded-full border border-white"
-            />
-          </NavLink>
-          <NavLink to="/">
-            <img
-              src="https://assets.jijistatic.net/static/img/footer-anchors/tanzania.svg"
-              alt="tanzania"
-              className="w-8 h-8 rounded-full border border-white"
-            />
-          </NavLink>
-          <NavLink to="/">
-            <img
-              src="https://assets.jijistatic.net/static/img/footer-anchors/ghana.svg"
-              alt="ghana"
-              className="w-8 h-8 rounded-full border border-white"
-            />
-          </NavLink>
-          <NavLink to="/">
-            <img
-              src="https://assets.jijistatic.net/static/img/footer-anchors/ethiopia.svg"
-              alt="ethiopia"
-              className="w-8 h-8 rounded-full border border-white"
-            />
-          </NavLink>
-          <NavLink to="/">
-            <img
-              src="https://assets.jijistatic.net/static/img/footer-anchors/senegal.svg"
-              alt="senegal"
-              className="w-8 h-8 rounded-full border border-white"
-            />
-          </NavLink>
-          <NavLink to="/">
-            <img
-              src="https://assets.jijistatic.net/static/img/footer-anchors/civ.svg"
-              alt="côte d'ivoire"
-              className="w-8 h-8 rounded-full border border-white"
-            />
-          </NavLink>
-          <NavLink to="/">
-            <img
-              src="https://assets.jijistatic.net/static/img/footer-anchors/bangladesh.svg"
-              alt="bangladesh"
-              className="w-8 h-8 rounded-full border border-white"
-            />
-          </NavLink>
-          <NavLink to="/">
-            <img
-              src="https://assets.jijistatic.net/static/img/footer-anchors/srilanka.svg"
-              alt="sri lanka"
-              className="w-8 h-8 rounded-full border border-white"
-            />
-          </NavLink>
-          <NavLink to="/">
-            <img
-              src="https://assets.jijistatic.net/static/img/footer-anchors/congo.svg"
-              alt="congo"
-              className="w-8 h-8 rounded-full border border-white"
-            />
-          </NavLink>
-          <NavLink to="/">
-            <img
-              src="https://assets.jijistatic.net/static/img/footer-anchors/repcongo.svg"
-              alt="Congo"
-              className="w-8 h-8 rounded-full border border-white"
-            />
-          </NavLink>
-          <NavLink to="/">
-            <img
-              src="https://assets.jijistatic.net/static/img/footer-anchors/pakistan.svg"
-              alt="pakistan"
-              className="w-8 h-8 rounded-full border border-white"
-            />
-          </NavLink>
-        </div>
 
+          {/* Our Apps */}
+          <div className="flex flex-col items-center md:items-start">
+            <h3 className="font-bold mb-4">Our Apps</h3>
+            {/* Apple Store */}
+            <a
+              href="https://apps.apple.com/us/app/jiji-nigeria/id966165025"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mb-4"
+            >
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Download_on_the_App_Store_Badge.svg"
+                alt="Download on App Store"
+                className="w-30 hover:scale-105 transition-transform duration-300"
+              />
+            </a>
+
+            {/* Google Play */}
+            <a
+              href="https://play.google.com/store/apps/details?id=ng.jiji.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
+                alt="Get it on Google Play"
+                className="w-30 hover:scale-105 transition-transform duration-300"
+              />
+            </a>
+          </div>
+
+          {/* Our Resources */}
+          <div>
+            <h3 className="font-bold mb-4">Our Resources</h3>
+            <ul className="space-y-2">
+              {[
+                {
+                  text: "Jiji on FB",
+                  link: "https://www.facebook.com/Jiji.Nigeria",
+                },
+                {
+                  text: "Our Instagram",
+                  link: "https://www.instagram.com/jijinigeria",
+                },
+                {
+                  text: "Our YouTube",
+                  link: "https://www.youtube.com/channel/UCvAyphNspDQMFnp7g57jH-g",
+                },
+                {
+                  text: "Our Twitter",
+                  link: "https://twitter.com/jijinigeria",
+                },
+              ].map((item, index) => (
+                <li key={index}>
+                  <NavLink
+                    to={item.link}
+                    target={item.link.startsWith("http") ? "_blank" : "_self"}
+                    rel={
+                      item.link.startsWith("http") ? "noopener noreferrer" : ""
+                    }
+                    className="hover:text-blue-900 transition"
+                  >
+                    {item.text}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {/* Hot Links */}
+          <div>
+            <h3 className="font-bold mb-4">Hot Links</h3>
+            <ul className="space-y-2">
+              <li>
+                <NavLink
+                  to="https://jiji.africa/"
+                  className="hover:text-blue-900 transition"
+                >
+                  Jiji Africa
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* Flags Section */}
+        <div className="flex flex-wrap justify-center items-center gap-4 mb-6">
+          {[
+            {
+              text: "Kenya",
+              site: "https://jiji.co.ke/",
+              flag: "https://assets.jijistatic.net/static/img/footer-anchors/kenya.svg",
+            },
+            {
+              text: "Uganda",
+              site: "https://jiji.ug/",
+              flag: "https://assets.jijistatic.net/static/img/footer-anchors/uganda.svg",
+            },
+            {
+              text: "Tanzania",
+              site: "https://jiji.co.tz/",
+              flag: "https://assets.jijistatic.net/static/img/footer-anchors/tanzania.svg",
+            },
+            {
+              text: "Ghana",
+              site: "https://jiji.com.gh/",
+              flag: "https://assets.jijistatic.net/static/img/footer-anchors/ghana.svg",
+            },
+            {
+              text: "Ethiopia",
+              site: "https://jiji.com.et/",
+              flag: "https://assets.jijistatic.net/static/img/footer-anchors/ethiopia.svg",
+            },
+            {
+              text: "Senegal",
+              site: "https://jiji.sn/",
+              flag: "https://assets.jijistatic.net/static/img/footer-anchors/senegal.svg",
+            },
+            {
+              text: "Côte d'Ivoire",
+              site: "https://jiji.co.ci/",
+              flag: "https://assets.jijistatic.net/static/img/footer-anchors/civ.svg",
+            },
+            {
+              text: "Bangladesh",
+              site: "https://jiji-bd.com/",
+              flag: "https://assets.jijistatic.net/static/img/footer-anchors/bangladesh.svg",
+            },
+            {
+              text: "Sri Lanka",
+              site: "https://jiji.lk/",
+              flag: "https://assets.jijistatic.net/static/img/footer-anchors/srilanka.svg",
+            },
+            {
+              text: "Congo",
+              site: "https://jiji.cd/",
+              flag: "https://assets.jijistatic.net/static/img/footer-anchors/congo.svg",
+            },
+            {
+              text: "Rep. of Congo",
+              site: "https://jiji.cg/",
+              flag: "https://assets.jijistatic.net/static/img/footer-anchors/repcongo.svg",
+            },
+            {
+              text: "Pakistan",
+              site: "https://milay.pk/",
+              flag: "https://assets.jijistatic.net/static/img/footer-anchors/pakistan.svg",
+            },
+          ].map((country, index) => (
+            <NavLink
+              key={index}
+              to={country.site}
+              rel="noopener noreferrer"
+              className="group flex flex-col items-center"
+            >
+              <img
+                src={country.flag}
+                alt={country.text}
+                className="w-8 h-8 rounded-full border border-white hover:scale-105 transition-transform"
+              />
+            </NavLink>
+          ))}
+        </div>
         {/* Copyright */}
-        <div className="text-center text-white text-sm mt-4">
+        <div className="text-center text-white text-sm">
           &copy; {new Date().getFullYear()} Jiji.ng
         </div>
       </div>
