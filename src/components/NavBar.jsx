0df25dfde7a7901521f00205e3412b40a89eaf90
Wallychi <szetrov@gmail.com>
2025-10-16 01:54:20 +0100

    fix Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,46 @@
 import React from 'react'
 import { FaUser } from 'react-icons/fa'
 import { ImNewspaper } from 'react-icons/im'
-import { IoMdBookmark } from 'react-icons/io'
+import { IoMdBookmark, IoMdSettings } from 'react-icons/io'
 import { IoNotifications } from 'react-icons/io5'
 import { MdMessage } from 'react-icons/md'
 import Button from './Button'
+import { LuLogOut, LuShoppingBasket } from 'react-icons/lu'
+import { CgComment } from 'react-icons/cg'
+import { RiBarChart2Fill } from 'react-icons/ri'
 
 
 const NavBar = () => {
-  return (
-    <section className='flex items-center bg-primary p-2 justify-between px-30 border-b-1 border-b-gray-500'>
-        <h1 className='text-white text-4xl font-bold'>Jiji</h1>
+    return (
+        <section className='flex items-center bg-primary p-2 justify-between px-30 border-b-1 border-b-gray-500'>
+            <h1 className='text-white text-4xl font-bold'>Jiji</h1>
 
-        <div className='flex items-center gap-5'>
-            <div className='p-1.5 bg-white rounded-full'><IoMdBookmark color='grey' size={25} /></div>
-            <div className='p-1.5 bg-white rounded-full'><MdMessage color='grey' size={25} /></div>
-            <div className='p-1.5 bg-white rounded-full'><IoNotifications color='grey' size={25} /></div>
-            <div className='p-1.5 bg-white rounded-full'><ImNewspaper color='grey' size={25} /></div>
-            <div className='p-1.5 bg-gray-500 rounded-full border-1 border-white'><FaUser color='white' size={25} /></div>
-            <Button name="SELL" style={'px-10 bg-navbutton text-white font-semibold cursor-pointer'} />
-        </div>
-    </section>
-  )
+            <div className='flex items-center gap-5'>
+                <div className='relative group p-1.5 bg-white rounded-full cursor-pointer'><IoMdBookmark color='grey' size={25} />
+                    <span className='absolute text-sm top-10 -left-2 mt-2 hidden group-hover:block bg-gray-500 rounded-xl text-white px-2 whitespace-nowrap'>Saved</span>
+                </div>
+                <div className='relative group p-1.5 bg-white rounded-full cursor-pointer'><MdMessage color='grey' size={25} />
+                    <span className='absolute text-sm rounded-xl top-10 -left-5 mt-2 hidden group-hover:block bg-gray-500 text-white px-2 whitespace-nowrap'>Messages</span>
+                </div>
+                <div className='relative group p-1.5 bg-white rounded-full cursor-pointer'><IoNotifications color='grey' size={25} />
+                    <span className='absolute text-sm rounded-xl top-10 -left-6 mt-2 hidden group-hover:block bg-gray-500 text-white px-2 whitespace-nowrap'>Notifications</span>
+                </div>
+                <div className='relative group p-1.5 bg-white rounded-full cursor-pointer'><ImNewspaper color='grey' size={25} />
+                    <span className='absolute text-sm rounded-xl top-10 -left-6 mt-2 hidden group-hover:block bg-gray-500 text-white px-2 whitespace-nowrap'>My Adverts</span>
+                </div>
+                <div className='relative group p-1.5 bg-gray-500 rounded-full border-1 border-white cursor-pointer'><FaUser color='white' size={25} />
+                    <div className='absolute top-full left-0 -translate-x-1 mt-2 hidden cursor-pointer group-hover:flex group-hover:flex-col transition-all duration-1000 ease-in-out translate-y-1 group-hover:translate-y-0 bg-white rounded-md'>
+                        <div className='flex items-center border-b-1 hover:bg-gray-200 border-b-gray-300 px-5 py-3 gap-2 whitespace-nowrap rounded-t-md'><div><LuShoppingBasket size={25} /></div>My shop</div>
+                        <div className='flex items-center border-b-1 hover:bg-gray-200 border-b-gray-300 px-5 py-3 gap-3 whitespace-nowrap'><div><CgComment size={25} /></div>Feedback</div>
+                        <div className='flex items-center border-b-1 hover:bg-gray-200 border-b-gray-300 px-5 py-3 gap-3 whitespace-nowrap'><div><RiBarChart2Fill size={25} /></div>Performance</div>
+                        <div className='flex items-center border-b-1 hover:bg-gray-200 border-b-gray-300 px-5 py-3 gap-3 whitespace-nowrap'><div><IoMdSettings size={25}/></div>Settings</div>
+                        <div className='flex items-center px-5 py-3  hover:bg-gray-200 gap-3 whitespace-nowrap rounded-b-md'><div><LuLogOut size={25} /></div>Log out</div>
+                    </div>
+                </div>
+                <Button name="SELL" style={'px-10 bg-navbutton hover:bg-navbutton/90 text-white font-semibold cursor-pointer'} />
+            </div>
+        </section>
+    )
 }
 
 export default NavBar
