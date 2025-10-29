d588ca31d2c74d6e6f1e5dd421756e33922d7cbb
Adebiyi Ridwan <donrealex11@gmail.com>
2025-10-29 01:07:09 +0100

    UPDATE: sideBar vehicle hover

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,28 +1,58 @@
 import React from 'react'
+import { vehicleData } from '../constant/data'
 
 const SideBar = ({ style }) => {
   return (
     <section className={`${style} font-jiji`}>
-      <div className='absolute overflow-y-scroll top-10 h-120 left-30 flex flex-col bg-white rounded-lg'>
-        <div className='p-2 border-b-1 border-searchbg'>Vehicles</div>
-        <div className='p-2 border-b-1 border-searchbg'>Property</div>
-        <div className='p-2 border-b-1 border-searchbg'>Mobile Phones and Tablet</div>
-        <div className='p-2 border-b-1 border-searchbg'>Electronics</div>
-        <div className='p-2 border-b-1 border-searchbg'>Home, Furniture and Appliances</div>
-        <div className='p-2 border-b-1 border-searchbg'>Fashion</div>
-        <div className='p-2 border-b-1 border-searchbg'>Beauty and Personal Care</div>
-        <div className='p-2 border-b-1 border-searchbg'>Services</div>
-        <div className='p-2 border-b-1 border-searchbg'>Repair and Construction</div>
-        <div className='p-2 border-b-1 border-searchbg'>Commercial Equipment and Tools</div>
-        <div className='p-2 border-b-1 border-searchbg'>Leisure and Activities</div>
-        <div className='p-2 border-b-1 border-searchbg'>Babies and Kids</div>
-        <div className='p-2 border-b-1 border-searchbg'>Food, Agriculture and Farming</div>
-        <div className='p-2 border-b-1 border-searchbg'>Animals and Pets</div>
-        <div className='p-2 border-b-1 border-searchbg'>Jobs</div>
-        <div className='p-2 border-b-1 border-searchbg'>Seeking Work-CVs</div>
+      <div className="absolute top-10 h-120 p-2 left-95 -translate-x-1 mt-2 hidden cursor-pointer group-hover:flex group-hover:flex-col transition-all duration-1000 ease-in-out translate-y-1 group-hover:translate-y-0 bg-white rounded-md overflow-y-scroll">
+        <div className="">
+          {vehicleData.map((item) => (
+            <div className="flex items-center p-2 hover:bg-searchbg cursor-pointer">
+              <img src={item.img} alt="car" className="w-10 h-10 mr-2" />
+              <div className="flex flex-col">
+                <h1 className="flex-1">{item.contents}</h1>
+                <p className="text-gray-400">{item.num}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div  className="relative group" >
+      <div className="absolute overflow-y-scroll top-10 h-120 left-30 flex flex-col bg-white rounded-lg">
+        <div className="p-2 border-b-1 border-searchbg group-hover:flex cursor-pointer"> Vehicles</div>
+        <div className="p-2 border-b-1 border-searchbg">Property</div>
+        <div className="p-2 border-b-1 border-searchbg">
+          Mobile Phones and Tablet
+        </div>
+        <div className="p-2 border-b-1 border-searchbg">Electronics</div>
+        <div className="p-2 border-b-1 border-searchbg">
+          Home, Furniture and Appliances
+        </div>
+        <div className="p-2 border-b-1 border-searchbg">Fashion</div>
+        <div className="p-2 border-b-1 border-searchbg">
+          Beauty and Personal Care
+        </div>
+        <div className="p-2 border-b-1 border-searchbg">Services</div>
+        <div className="p-2 border-b-1 border-searchbg">
+          Repair and Construction
+        </div>
+        <div className="p-2 border-b-1 border-searchbg">
+          Commercial Equipment and Tools
+        </div>
+        <div className="p-2 border-b-1 border-searchbg">
+          Leisure and Activities
+        </div>
+        <div className="p-2 border-b-1 border-searchbg">Babies and Kids</div>
+        <div className="p-2 border-b-1 border-searchbg">
+          Food, Agriculture and Farming
+        </div>
+        <div className="p-2 border-b-1 border-searchbg">Animals and Pets</div>
+        <div className="p-2 border-b-1 border-searchbg">Jobs</div>
+        <div className="p-2 border-b-1 border-searchbg">Seeking Work-CVs</div>
+      </div>
       </div>
     </section>
-  )
+  );
 }
 
 export default SideBar
---
+++ src/constant/data.jsx
@@ -0,0 +1,50 @@
+import carParts from "../../public/assets/carParts.png"
+import cars from "../../public/assets/cars.png"
+import moto from "../../public/assets/moto.png"
+import bus from "../../public/assets/bus.png"
+import trucks from "../../public/assets/trucks.png"
+import heavy from "../../public/assets/heavy.png"
+import services from "../../public/assets/services.png"
+
+
+export const vehicleData = [
+   { 
+    img: carParts,
+    contents: "Vehicle Parts & Accessories",
+    num: "202 881 ads"
+},
+
+   { img: cars,
+    contents: "Cars",
+    num: "121 963 ads"},
+    {
+    img: moto,
+    contents: "Motorcycles & Scooters",
+    num: "3412 ads"
+   },
+   {
+    img: bus,
+    contents: "Buses & Microbuses",
+    num: "262 ads"
+   },
+   {
+    img: trucks,
+    contents: "Trucks & Trailers",
+    num: "465 ads"
+   },
+   {
+    img: heavy, 
+    contents: "Construction & Heavy Machinery",
+    num: "211 ads"
+   },
+   {
+    img: services,
+    contents: "Watercraft & Boats",
+    num: "0 ads"
+   },
+   {
+    img: services,
+    contents: "Car Services",
+    num: "1269 ads"
+   }
+   ]
