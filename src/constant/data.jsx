5ab6e7b20c1ce992199ec599fb8fefb58d30ae3d
Wallychi <szetrov@gmail.com>
2025-10-30 02:38:14 +0100

    Hidden SideBar Now Hoverable

--- src/constant/data.jsx
+++ src/constant/data.jsx
@@ -8,43 +8,93 @@ import services from "../../public/assets/services.png"
 
 
 export const vehicleData = [
-   { 
-    img: carParts,
-    contents: "Vehicle Parts & Accessories",
-    num: "202 881 ads"
-},
+   {
+      img: carParts,
+      contents: "Vehicle Parts & Accessories",
+      num: "202 881 ads"
+   },
 
-   { img: cars,
-    contents: "Cars",
-    num: "121 963 ads"},
-    {
-    img: moto,
-    contents: "Motorcycles & Scooters",
-    num: "3412 ads"
+   {
+      img: cars,
+      contents: "Cars",
+      num: "121 963 ads"
    },
    {
-    img: bus,
-    contents: "Buses & Microbuses",
-    num: "262 ads"
+      img: moto,
+      contents: "Motorcycles & Scooters",
+      num: "3412 ads"
    },
    {
-    img: trucks,
-    contents: "Trucks & Trailers",
-    num: "465 ads"
+      img: bus,
+      contents: "Buses & Microbuses",
+      num: "262 ads"
    },
    {
-    img: heavy, 
-    contents: "Construction & Heavy Machinery",
-    num: "211 ads"
+      img: trucks,
+      contents: "Trucks & Trailers",
+      num: "465 ads"
    },
    {
-    img: services,
-    contents: "Watercraft & Boats",
-    num: "0 ads"
+      img: heavy,
+      contents: "Construction & Heavy Machinery",
+      num: "211 ads"
    },
    {
-    img: services,
-    contents: "Car Services",
-    num: "1269 ads"
+      img: services,
+      contents: "Watercraft & Boats",
+      num: "0 ads"
+   },
+   {
+      img: services,
+      contents: "Car Services",
+      num: "1269 ads"
    }
-   ]
+]
+export const propertyData = [
+   {
+      img: carParts,
+      contents: "New Builds",
+      num: "5 426 ads"
+   },
+
+   {
+      img: cars,
+      contents: "House & Apartment For Rent",
+      num: "25 782 ads"
+   },
+   {
+      img: moto,
+      contents: "House & Apartment For Sale",
+      num: "43 786 ads"
+   },
+   {
+      img: moto,
+      contents: "Short Let",
+      num: "7 873 ads"
+   },
+   {
+      img: moto,
+      contents: "Land & Plot For Rent",
+      num: "296 ads"
+   },
+   {
+      img: moto,
+      contents: "Land & Plot For Sale",
+      num: "21 531 ads"
+   },
+   {
+      img: moto,
+      contents: "Event Centres, Venues & Workstations",
+      num: "211 ads"
+   },
+   {
+      img: moto,
+      contents: "Commercial Property For Rent",
+      num: "3 670 ads"
+   },
+   {
+      img: moto,
+      contents: "Commercial Property For Sale",
+      num: "4 574 ads"
+   },
+]
