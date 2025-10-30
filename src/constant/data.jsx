520cc2e081e3639d8af79312c49b926c77c7c9f0
Adebiyi Ridwan <donrealex11@gmail.com>
2025-10-30 22:27:06 +0100

    UPADTE: mobile phone and electronics done

--- src/constant/data.jsx
+++ src/constant/data.jsx
@@ -6,23 +6,53 @@ import trucks from "../../public/assets/trucks.png"
 import heavy from "../../public/assets/heavy.png"
 import services from "../../public/assets/services.png"
 
+import builds from "../../public/assets/builds.png"
+import houseRent from "../../public/assets/houseRent.png"
+import houseSale from "../../public/assets/houseSale.png"
+import shortLet from "../../public/assets/shortLet.png"
+import landRent from "../../public/assets/landRent.png"
+import landSale from "../../public/assets/landSale.png"
+import eventCenters from "../../public/assets/eventCenters.png"
+import propertyRent from "../../public/assets/propertyRent.png"
+import propertySale from "../../public/assets/propertySale.png"
+
+import phones from "../../public/assets/phones.png"
+import accessories from "../../public/assets/accessories.png"
+import watches from "../../public/assets/watches.png"
+import tablets from "../../public/assets/tablets.png"
+import headphones from "../../public/assets/headphones.png"
+
+import laptops from "../../public/assets/laptops.png"
+import tv from "../../public/assets/tv.png"
+import consoles from "../../public/assets/consoles.png"
+import audio from "../../public/assets/audio.png"
+import cameras from "../../public/assets/cameras.png"
+import security from "../../public/assets/security.png"
+import networking from "../../public/assets/networking.png"
+import printers from "../../public/assets/printers.png"
+import computer from "../../public/assets/computer.png"
+import accessoriesPc from "../../public/assets/accessoriesPc.png"
+import hardware from "../../public/assets/hardware.png"
+import supplies from "../../public/assets/supplies.png"
+import game from "../../public/assets/game.png"
+import software from "../../public/assets/software.png"
 
 export const vehicleData = [
    {
       img: carParts,
       contents: "Vehicle Parts & Accessories",
-      num: "202 881 ads"
+      num: "202,881 ads"
    },
 
    {
       img: cars,
       contents: "Cars",
-      num: "121 963 ads"
+      num: "121,963 ads"
    },
    {
       img: moto,
       contents: "Motorcycles & Scooters",
-      num: "3412 ads"
+      num: "3,412 ads"
    },
    {
       img: bus,
@@ -47,54 +77,159 @@ export const vehicleData = [
    {
       img: services,
       contents: "Car Services",
-      num: "1269 ads"
+      num: "1,269 ads"
    }
 ]
 export const propertyData = [
    {
-      img: carParts,
+      img: builds,
       contents: "New Builds",
-      num: "5 426 ads"
+      num: "5,426 ads"
    },
 
    {
-      img: cars,
+      img: houseRent,
       contents: "House & Apartment For Rent",
-      num: "25 782 ads"
+      num: "25,782 ads"
    },
    {
-      img: moto,
+      img: houseSale,
       contents: "House & Apartment For Sale",
-      num: "43 786 ads"
+      num: "43,786 ads"
    },
    {
-      img: moto,
+      img: shortLet,
       contents: "Short Let",
-      num: "7 873 ads"
+      num: "7,873 ads"
    },
    {
-      img: moto,
+      img: landRent,
       contents: "Land & Plot For Rent",
       num: "296 ads"
    },
    {
-      img: moto,
+      img: landSale,
       contents: "Land & Plot For Sale",
-      num: "21 531 ads"
+      num: "21,531 ads"
    },
    {
-      img: moto,
+      img: eventCenters,
       contents: "Event Centres, Venues & Workstations",
       num: "211 ads"
    },
    {
-      img: moto,
+      img: propertyRent,
       contents: "Commercial Property For Rent",
-      num: "3 670 ads"
+      num: "3,670 ads"
    },
    {
-      img: moto,
+      img: propertySale,
       contents: "Commercial Property For Sale",
-      num: "4 574 ads"
+      num: "4,574 ads"
    },
 ]
+export const phoneData =[
+    {
+    img: phones,
+    contents: "Mobile Phones",
+    num: "6,884 ads"
+    },
+    {
+    img: accessories,
+    contents: "Accessories for Phones & Tablets",
+    num: "1,588 ads"
+    },
+    {
+        img: watches,
+    contents: "Smart Watches",
+    num: "1,208 ads"
+
+    },
+    {
+    img: tablets, 
+    contents: "Tablets",
+    num: "798 ads"
+    },
+    {
+    img: headphones,
+    contents: "Headphones",
+    num: "1,173 ads"
+    }
+]
+export const electronicsData = [
+  {
+    img: laptops,
+    contents: "Laptops & Computers",
+    num: "52,911 ads",
+  },
+  {
+    img: tv,
+    contents: "TV & DVD Equipment",
+    num: "39,387 ads",
+  },
+  {
+    img: consoles,
+    contents: "Video Games & Consoles",
+    num: "7,378 ads",
+  },
+  {
+    img: audio,
+    contents: "Audio & Music Equipment",
+    num: "49,958 ads",
+  },
+  {
+    img: headphones,
+    contents: "Headphones",
+    num: "2,345 ads",
+  },
+  {
+    img: cameras,
+    contents: "Photo & Video Cameras",
+    num: "6,299 ads",
+  },
+  {
+      img: security,
+      contents: "Security & Surveillance",
+      num: "17,787 ads",
+  },
+  {
+   img: networking,
+   contents: "Networking Products",
+   num: "13,513 ads",
+  },
+  {
+   img: printers,
+   contents: "Printers & Scanners",
+   num: "7,570 ads",
+  },
+  {
+   img: computer,
+   contents: "Computer Monitors",
+   num: "3,801 ads",
+  },
+  {
+   img: accessoriesPc,
+   contents: "Computer Hardware",
+   num: "5,306 ads",
+  },
+  {
+   img: hardware,
+   contents: "Computer Accessories",
+   num: "18,918 ads",
+  },
+  {
+   img: supplies,
+   contents: "Accessories & Supplies for Electronics",
+   num: "25,516 ads",
+  },
+  {
+   img: game,
+   contents: "Video Games",
+   num: "4,450 ads",
+  },
+  {
+   img: software,
+   contents: "Softwares",
+   num: "1,266 ads",
+  }
+];
