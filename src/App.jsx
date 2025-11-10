9f9b837038771851529a1393d9179cc106e614bf
Adebiyi Ridwan <donrealex11@gmail.com>
2025-11-10 16:54:49 +0100

    vercel analytic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Analytics />
       </Routes>
     </BrowserRouter>
   );
